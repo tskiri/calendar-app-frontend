4252ec677eb0718ac204c68eda1e9e88c767a47b
tskiri <t.kiribayashi@infomart.co.jp>
2026-06-08 10:38:09 +0900

    カレンダーのグリッドおよび予定を書く日付に割り振る機能を実装

---
+++ app/_component/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header style={{ padding: "20px", borderBottom: "1px solid #ccc", backgroundColor: "#f9f9f9" }}>
+      <h2>カレンダーアプリ</h2>
+    </header>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,40 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { useEffect, useState } from 'react';
+import { PlanData } from "./_interfaces/PlanData";
+import { findAllPlans } from "./api/plans";
+import Header from './_component/Header';
+import CalendarView from "./_component/CalendarView";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+
+const Home = () => {
+  // 画面が開いた瞬間。バックエンドから帰ってくるデータの格納場所を定義
+  const [plans, setPlans] = useState<PlanData[]>([]);
+
+  // useEffect：コンポーネントのマウント時やState変更時の副作用処理を記述
+  useEffect(() => {
+    const getPlans = async () => {
+      try {
+        // バックエンドにデータを要求
+        const response = await findAllPlans();
+        // 状態を更新
+        setPlans(response);
+      } catch (error) {
+        console.error('予定の取得に失敗しました:', error);
+      }
+    };
+    getPlans();
+  }, []);
+
+  // UIの記述
+return (
+    <div>
+      <Header />
+      
+      <main>
+        <CalendarView plans={plans} />
       </main>
     </div>
   );
 }
+
+export default Home;
